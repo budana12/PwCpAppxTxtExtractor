80e66fb301fcce29ee381af00927846a72f706be
budana12 <sirowabudana@gmail.com>
2025-05-04 22:52:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "10768857"
 api_hash = "81b5d675d410f16ceb39df10db9cdb54"
-bot_token = "7464498219:AAFINfJVosIf-pvUxNJAEa0uoHGxaSM2j5M"
+bot_token = "7464498219:AAFacXLo-pvsNBWog8jgXtd3YU0pLukVvJ0"
 auth_users = [6520378417]
